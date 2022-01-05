37be8799e3c0b7019bad7c5b03d234bbec643ebd
dangkhoa1209 <hdkhoa.work@gmail.com>
2022-01-05 09:36:13 +0700

    Fix run at 2 o'clock

--- scripts/sort_top_trending.js
+++ scripts/sort_top_trending.js
@@ -3,38 +3,29 @@ const schedule = require("node-schedule");
 const sortTopTrending = require("../app/consoles/sortTopTrending");
 
 function runSortTopTrending(){
-    sortTopTrending.sort(function (err, result) {
-        if (err) {
-            console.log(err);
-        }
-
-        if(result){
-            console.log("Done sort");
-        }
-        
-    });
-    return "";
+    sortTopTrending.sort()   
 };
-
-//runSortTopTrending();
-
-// schedule.scheduleJob({hour: 2, minute: 00}, function () {
-//     console.log("Chờ run");
-//     runSortTopTrending();
-// });
-
+    
 console.log("Waiting to run");
 var index = 0;
+
+
+//Chay moi 15giay
+// schedule.scheduleJob("*/15 * * * * *", function() {
+//     index += 1;
+//     runSortTopTrending();
+//     console.log("Waiting to run " + index);
+// });
+
+
+//Chy luc 2h moi ngay
 schedule.scheduleJob({hour: 2, minute: 00}, function() {
     index += 1;
     runSortTopTrending();
     console.log("Waiting to run " + index);
 });
 
-// console.log("Waiting to run");
-// var index = 0;
-// schedule.scheduleJob("*/30 * * * * *", function() {
-//     index += 1;
-//     runSortTopTrending();
-//     console.log("Waiting to run " + index);
-// });
+
+
+
+
